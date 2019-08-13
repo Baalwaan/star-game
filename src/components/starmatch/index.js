f2a8e14a028e9f66deb07701921e754dc1f2c53c
Burhan Baalwaan <42354400+Baalwaan@users.noreply.github.com>
2019-08-13 14:17:46 +0100

    create onNumberClick function with logic

--- src/components/starmatch/index.js
+++ src/components/starmatch/index.js
@@ -11,7 +11,11 @@ import { sum, range, random, randomSumIn } from '../../utils';
 
 const StarMatch = () => {
   const [stars, setStars] = React.useState(random(1, 9));
+<<<<<<< Updated upstream
   const [availableNums, setAvailableNums] = React.useState([utils.range(1, 9)]);
+=======
+  const [availableNums, setAvailableNums] = React.useState(range(1, 9));
+>>>>>>> Stashed changes
   const [candidateNums, setCandidateNums] = React.useState([]);
 
   const candidatesAreWrong = sum(candidateNums) > stars;
@@ -26,6 +30,31 @@ const StarMatch = () => {
     }
 
     return 'available';
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if (currentStatus === 'used') {
+      console.log('used');
+      return;
+    }
+
+    //candidateNumbs
+    const newCandidateNums = candidateNums.concat(number);
+
+    if (sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+      console.log('if');
+    } else {
+      console.log('else');
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+
+      setStars(randomSumIn(newAvailableNums, 9)); // //redraw number of stars. only redraw number of stars that are still playable
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+    //currentStatus => newStatus
   };
 
   //candidateNUms
@@ -47,6 +76,7 @@ const StarMatch = () => {
               key={number}
               number={number}
               status={numberStatus(number)}
+              onClick={onNumberClick}
             />
           ))}
         </div>
